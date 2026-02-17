26583f35fddb019ea05787389d832317a794ef6c
martin <2912519304@qq.com>
2026-02-18 02:28:20 +0800

    add

--- back-end/demo/src/main/java/com/example/demo/UserController.java
+++ back-end/demo/src/main/java/com/example/demo/UserController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
-@Tag(name = "用户管理", description = "处理用户登录、注册及权限校验")
 public class UserController {
 
-    @Operation(summary = "获取用户信息", description = "根据 ID 查询用户的详细资料")
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
-    public String getUser(@PathVariable String id) {
-        return id;
-    }
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUserById(id);
 
-    @GetMapping("/test-bug")
-    public String triggerBug() {
-
-        return "Bug is not live";
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
