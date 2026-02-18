105401ea4f6c71634c717b461b0e8ae59272514e
martin <2912519304@qq.com>
2026-02-18 14:16:34 +0800

    add cover

--- frontend-nuxt/vitest.config.ts
+++ frontend-nuxt/vitest.config.ts
@@ -5,5 +5,12 @@ export default defineConfig({
         // 允许在测试文件中直接使用 describe, it, expect 而不需要手动 import
         globals: true,
         environment: 'happy-dom',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov'], // 必须包含 lcov 格式
+            reportsDirectory: './coverage', // 报告生成的位置
+            // 建议排除掉不需要统计覆盖率的目录
+            exclude: ['node_modules/**', '.nuxt/**', 'dist/**', 'tests/**']
+        }
     },
 })
